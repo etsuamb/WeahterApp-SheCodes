55ff253fdd02fa8ce5fed46728fdf0012d05ce4d
Etsub <etsubgirma0312@gmail.com>
2024-07-26 04:55:56 -0900

    Added temp from API

--- src/script.js
+++ src/script.js
@@ -1,8 +1,20 @@
-function searchReuslt(event) {
+function refreshCity(response){
+let temperatureValue = document.querySelector("#temp-value");
+let temperature = response.data.temperature.current;
+temperatureValue.innerHTML = Math.round(temperature);
+let cityElement = document.querySelector("#city-name");
+cityElement.innerHTML = response.data.city;
+}
+function searchCity(city){
+let apiKey = "0c0da2fc4to069493aa82b3dbf389dd0";
+let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
+axios.get(apiUrl).then(refreshCity);
+}
+function searchResult(event) {
   event.preventDefault();
   let inputElement = document.querySelector("#search-input");
-  let h1 = document.querySelector("#city-name");
-  h1.innerHTML = inputElement.value;
+  searchCity(inputElement.value);
 }
 let searchElement = document.querySelector("#input-form");
-searchElement.addEventListener("submit", searchReuslt);
+searchElement.addEventListener("submit", searchResult);
+searchCity("Addis Ababa");
